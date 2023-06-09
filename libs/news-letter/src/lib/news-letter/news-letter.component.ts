003ed447e4916b9ee4874a02b41dbafa062a9690
Kate Sky <kateskytech@gmail.com>
2023-06-08 20:39:29 -0500

    adding ngrx

--- libs/news-letter/src/lib/news-letter/news-letter.component.ts
+++ libs/news-letter/src/lib/news-letter/news-letter.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { GlobalStoreModule, addUser, increment, reset } from '@org/global-store';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'org-news-letter',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GlobalStoreModule],
   templateUrl: './news-letter.component.html',
   styleUrls: ['./news-letter.component.scss'],
+
 })
 export class NewsLetterComponent {
-
+  constructor(private readonly store: Store<{ count: number }>) {
+    this.store.dispatch(reset());
+  }
   @Input() show = true;
+  add(username: string) {
+    this.store.dispatch(increment());
+  }
 }
+
+
