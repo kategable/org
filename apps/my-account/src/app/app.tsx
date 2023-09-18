6c110623b454563ce75b0bb0365c5956a988d828
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-09-18 18:28:57 -0500

    added user accout icon

--- apps/my-account/src/app/app.tsx
+++ apps/my-account/src/app/app.tsx
@@ -18,11 +18,11 @@ export function App() {
       <br />
       <div role="navigation">
         <ul>
-          <li>
-            <Link to="/">Home</Link>
+          <li> 
+            <Link to="/">Home</Link> 
           </li>
           <li>
-            <Link to="/page-2">Page 2</Link>
+            <Link to="/page-2">Page 2</Link> 
           </li>
         </ul>
       </div>
